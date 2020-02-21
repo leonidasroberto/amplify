6dd78f8c07eb3a790922f231fe98693e6e6ec4cd
leonidas <leonidasroberto01@hotmail.com>
2020-02-21 07:52:17 -0300

    resolved ?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './storage/Storage';
 import { Provider } from 'react-redux';
